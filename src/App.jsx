b438b79c1bb02799930ec5f6d7e46bc4bf305ec3
Yoda1221 <bfekete66@gmail.com>
2022-08-27 19:14:18 +0200

    Create Posts with Author, Reactions buttons, and Date

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Counter from "./features/counter/Counter"
+import PostsList from "./features/posts/PostsList"
+import AddPost from "./features/posts/AddPost"
 
 function App() {
   return (
     <main className="App">
-      <Counter />
+      <AddPost />
+      <PostsList />
     </main>
   )
 }
