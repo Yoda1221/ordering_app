6b32a1200ee93677f1a0b3301b6d9808a32b888f
Yoda1221 <bfekete66@gmail.com>
2022-09-02 07:49:47 +0200

    Delete unused methods

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -1,4 +1,3 @@
-import { selectUsersIds, useGetUserQuery } from './usersSlice'
 import { Link }             from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectAllUsers } from './usersSlice'
